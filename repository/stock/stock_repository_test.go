befd72148966d227e568bc2501731f6e65345e12
RifqiF27 <rifqifadluloh27@gmail.com>
2024-12-09 04:13:26 +0700

    tests:testing stock repository

--- repository/stock/stock_repository_test.go
+++ repository/stock/stock_repository_test.go
@@ -1,186 +1,185 @@
 package stockrepository_test
 
-// import (
-// 	"dashboard-ecommerce-team2/models"
-// 	stockrepository "dashboard-ecommerce-team2/repository/stock"
-// 	"fmt"
-// 	"testing"
-// 	"time"
+import (
+	"dashboard-ecommerce-team2/repository/stock"
+	"errors"
+	"testing"
 
-// 	"github.com/DATA-DOG/go-sqlmock"
-// 	"github.com/stretchr/testify/assert"
-// 	"go.uber.org/zap"
-// 	"gorm.io/driver/postgres"
-// 	"gorm.io/gorm"
-// 	"gorm.io/gorm/logger"
-// )
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
-// func setupTestDB(t *testing.T) (*gorm.DB, sqlmock.Sqlmock, func()) {
-// 	mockDB, mock, err := sqlmock.New()
-// 	if err != nil {
-// 		t.Fatalf("Failed to create mock database: %v", err)
-// 	}
+func SetupTestDB() (*gorm.DB, sqlmock.Sqlmock) {
 
-// 	dialector := postgres.New(postgres.Config{
-// 		Conn: mockDB,
-// 	})
-// 	db, err := gorm.Open(dialector, &gorm.Config{
-// 		Logger: logger.Default.LogMode(logger.Silent), // Menonaktifkan logging di pengujian
-// 	})
-// 	if err != nil {
-// 		t.Fatalf("Failed to open gorm DB: %v", err)
-// 	}
+	db, mock, _ := sqlmock.New()
+	dialector := postgres.New(postgres.Config{
+		Conn:       db,
+		DriverName: "postgres",
+	})
+	gormDB, _ := gorm.Open(dialector, &gorm.Config{})
+	return gormDB, mock
+}
 
-// 	// Mengambil objek sql.DB dan membuat fungsi untuk menutup koneksi.
-// 	sqlDB, err := db.DB()
-// 	if err != nil {
-// 		t.Fatalf("Failed to get underlying sql.DB: %v", err)
-// 	}
+func TestDelete(t *testing.T) {
 
-// 	// Fungsi untuk menutup koneksi DB
-// 	cleanup := func() {
-// 		sqlDB.Close()
-// 	}
+	logger, _ := zap.NewDevelopment()
+	db, mock := SetupTestDB()
 
-// 	return db, mock, cleanup
-// }
+	repo := stockrepository.NewStockRepository(db, logger)
 
-// func TestDelete(t *testing.T) {
-// 	// Mock DB and Logger
-// 	db, mock, err := sqlmock.New()
-// 	assert.NoError(t, err)
-// 	defer db.Close()
+	mock.ExpectQuery(`SELECT \* FROM "stocks" WHERE "stocks"."id" = \$1 ORDER BY "stocks"."id" LIMIT \$2`).
+		WithArgs(1, 1).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
 
-// 	gormDB, err := gorm.Open(postgres.New(postgres.Config{
-// 		Conn: db,
-// 	}), &gorm.Config{})
-// 	assert.NoError(t, err)
+	mock.ExpectQuery(`SELECT \* FROM "stocks" WHERE "stocks"."id" = \$1 ORDER BY "stocks"."id" LIMIT \$2`).
+		WithArgs(999, 1).
+		WillReturnError(gorm.ErrRecordNotFound)
 
-// 	logger, _ := zap.NewProduction()
+	mock.ExpectQuery(`SELECT \* FROM "stocks" WHERE "stocks"."id" = \$1 ORDER BY "stocks"."id" LIMIT 1`).
+		WithArgs(1).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+	mock.ExpectExec(`DELETE FROM "stocks" WHERE "stocks"."id" = \$1`).
+		WithArgs(1).
+		WillReturnError(errors.New("some error"))
+	err := repo.Delete(1)
+	assert.Error(t, err)
+}
+func TestGetByID(t *testing.T) {
 
-// 	repo := stockrepository.NewStockRepository(gormDB, logger)
+	logger, _ := zap.NewDevelopment()
+	db, mock := SetupTestDB()
 
-// 	t.Run("successfully delete stock history", func(t *testing.T) {
-// 		mock.ExpectQuery("SELECT * FROM \"stocks\" WHERE \"stocks\".\"id\" = \\$1 ORDER BY \"stocks\".\"id\" LIMIT \\$2").
-// 			WithArgs(1, 1).
-// 			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+	repo := stockrepository.NewStockRepository(db, logger)
 
-// 		mock.ExpectExec("DELETE FROM \"stocks\" WHERE \"stocks\".\"id\" = \\$1").
-// 			WithArgs(1).
-// 			WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectQuery(`SELECT stocks.id, stocks.product_id, stocks.type, stocks.quantity, stocks.created_at, stocks.updated_at, products.name as product_name, CASE WHEN categories.variant = '{}' THEN NULL ELSE categories.variant END AS variant FROM "stocks" JOIN products ON products.id = stocks.product_id JOIN categories ON categories.id = products.category_id WHERE stocks.id = \$1 ORDER BY "stocks"."id" LIMIT \$2`).
+		WithArgs(1, 1).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "product_id", "type", "quantity", "product_name", "variant"}).
+			AddRow(1, 1, "in", 100, "Item A", nil))
 
-// 		err := repo.Delete(1)
-// 		assert.NoError(t, err)
-// 	})
+	stock, err := repo.GetByID(1)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, int(stock.ID))
+	assert.Equal(t, "Item A", stock.ProductName)
+	assert.Equal(t, 100, stock.Quantity)
 
-// 	t.Run("error when stock history not found", func(t *testing.T) {
-// 		mock.ExpectQuery("SELECT * FROM \"stocks\" WHERE \"stocks\".\"id\" = \\$1 ORDER BY \"stocks\".\"id\" LIMIT \\$2").
-// 			WithArgs(1, 1).
-// 			WillReturnError(fmt.Errorf("record not found"))
+	mock.ExpectQuery(`SELECT stocks.id, stocks.product_id, stocks.type, stocks.quantity, stocks.created_at, stocks.updated_at, products.name as product_name, CASE WHEN categories.variant = '{}' THEN NULL ELSE categories.variant END AS variant FROM "stocks" JOIN products ON products.id = stocks.product_id JOIN categories ON categories.id = products.category_id WHERE stocks.id = \$1 ORDER BY "stocks"."id" LIMIT \$2`).
+		WithArgs(999, 1).
+		WillReturnError(gorm.ErrRecordNotFound)
 
-// 		err := repo.Delete(1)
-// 		assert.EqualError(t, err, "history not found")
-// 	})
+	stock, err = repo.GetByID(999)
+	assert.Error(t, err)
+	assert.Nil(t, stock)
+}
 
-// 	t.Run("error on delete failure", func(t *testing.T) {
-// 		mock.ExpectQuery("SELECT * FROM \"stocks\" WHERE \"stocks\".\"id\" = \\$1 ORDER BY \"stocks\".\"id\" LIMIT \\$2").
-// 			WithArgs(1, 1).
-// 			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+// func TestStockRepository_Update_Postgres(t *testing.T) {
 
-// 		mock.ExpectExec("DELETE FROM \"stocks\" WHERE \"stocks\".\"id\" = \\$1").
-// 			WithArgs(1).
-// 			WillReturnError(fmt.Errorf("delete error"))
+// 	logger, _ := zap.NewDevelopment()
+// 	db, mock := SetupTestDB()
 
-// 		err := repo.Delete(1)
-// 		assert.EqualError(t, err, "delete error")
-// 	})
-// }
-
-// func TestGetByID(t *testing.T) {
-// 	// Mock DB and Logger
-// 	db, mock, err := sqlmock.New()
-// 	assert.NoError(t, err)
-// 	defer db.Close()
-
-// 	gormDB, err := gorm.Open(postgres.New(postgres.Config{
-// 		Conn: db,
-// 	}), &gorm.Config{})
-// 	assert.NoError(t, err)
-
-// 	logger, _ := zap.NewProduction()
-
-// 	repo := stockrepository.NewStockRepository(gormDB, logger)
-
-// 	t.Run("successfully fetch stock by ID", func(t *testing.T) {
-// 		mock.ExpectQuery("SELECT stocks.id, stocks.product_id, stocks.type, stocks.quantity, stocks.created_at, stocks.updated_at, products.name as product_name, CASE WHEN categories.variant = '{}' THEN NULL ELSE categories.variant END AS variant FROM \"stocks\" JOIN products ON products.id = stocks.product_id JOIN categories ON categories.id = products.category_id WHERE stocks.id = \\$1").
-// 			WithArgs(1).
-// 			WillReturnRows(sqlmock.NewRows([]string{"id", "product_id", "type", "quantity", "created_at", "updated_at", "product_name", "variant"}).
-// 				AddRow(1, 1, "in", 10, time.Now(), time.Now(), "Product Name", `{"size": "L"}`))
-
-// 		result, err := repo.GetByID(1)
-// 		assert.NoError(t, err)
-// 		assert.NotNil(t, result) // Pastikan result tidak nil
-
-// 		expectedVariant := map[string]string{"size": "L"}
-// 		assert.Equal(t, expectedVariant, result.Variant)
-// 	})
-
-// 	t.Run("error when stock history not found", func(t *testing.T) {
-// 		mock.ExpectQuery("SELECT stocks.id, stocks.product_id, stocks.type, stocks.quantity, stocks.created_at, stocks.updated_at, products.name as product_name, CASE WHEN categories.variant = '{}' THEN NULL ELSE categories.variant END AS variant FROM \"stocks\" JOIN products ON products.id = stocks.product_id JOIN categories ON categories.id = products.category_id WHERE stocks.id = \\$1").
-// 			WithArgs(1).
-// 			WillReturnError(fmt.Errorf("record not found"))
-
-// 		result, err := repo.GetByID(1)
-// 		assert.Error(t, err)
-// 		assert.Nil(t, result)
-// 	})
-
-// 	t.Run("error on fetch failure", func(t *testing.T) {
-// 		mock.ExpectQuery("SELECT stocks.id, stocks.product_id, stocks.type, stocks.quantity, stocks.created_at, stocks.updated_at, products.name as product_name, CASE WHEN categories.variant = '{}' THEN NULL ELSE categories.variant END AS variant FROM \"stocks\" JOIN products ON products.id = stocks.product_id JOIN categories ON categories.id = products.category_id WHERE stocks.id = \\$1").
-// 			WithArgs(1).
-// 			WillReturnError(fmt.Errorf("fetch error"))
-
-// 		result, err := repo.GetByID(1)
-// 		assert.Error(t, err)
-// 		assert.Nil(t, result)
-// 	})
-// }
-
-// func TestUpdate(t *testing.T) {
-// 	db, mock, cleanup := setupTestDB(t)
-// 	defer cleanup()
-
-// 	logger := zap.NewNop()
 // 	repo := stockrepository.NewStockRepository(db, logger)
 
-// 	t.Run("successfully update stock", func(t *testing.T) {
-// 		mock.ExpectQuery("SELECT stocks.id, stocks.product_id, stocks.type, stocks.quantity, stocks.created_at, stocks.updated_at, products.name as product_name, CASE WHEN categories.variant = '{}' THEN NULL ELSE categories.variant END AS variant FROM \"stocks\" JOIN products ON products.id = stocks.product_id JOIN categories ON categories.id = products.category_id WHERE stocks.id = \\$1").
-// 			WithArgs(1).
-// 			WillReturnRows(sqlmock.NewRows([]string{"id", "product_id", "type", "quantity", "created_at", "updated_at", "product_name", "variant"}).
-// 				AddRow(1, 1, "in", 10, time.Now(), time.Now(), "Product Name", `{"size": "L"}`))
+// 	tests := []struct {
+// 		name        string
+// 		input       *models.StockRequest
+// 		setupMocks  func()
+// 		expectedErr error
+// 	}{
+// 		{
+// 			name: "Successful stock update",
+// 			input: &models.StockRequest{
+// 				ProductID: 1,
+// 				Type:      "in",
+// 				Quantity:  10,
+// 			},
+// 			setupMocks: func() {
 
-// 		mock.ExpectExec("UPDATE \"products\" SET .* WHERE id = \\$1").
-// 			WithArgs(1).
-// 			WillReturnResult(sqlmock.NewResult(1, 1))
+// 				mock.ExpectQuery(`SELECT stock FROM products WHERE id = \?`).
+// 					WithArgs(1).
+// 					WillReturnRows(sqlmock.NewRows([]string{"stock"}).AddRow(5))
 
-// 		mock.ExpectExec("INSERT INTO \"stocks\" .*").
-// 			WillReturnResult(sqlmock.NewResult(1, 1))
+// 				mock.ExpectBegin()
 
-// 		err := repo.Update(&models.StockRequest{
-// 			ProductID: 1,
-// 			Type:      "in",
-// 			Quantity:  10,
+// 				mock.ExpectExec(`UPDATE products SET stock = \?, updated_at = \? WHERE id = \?`).
+// 					WithArgs(15, sqlmock.AnyArg(), 1).
+// 					WillReturnResult(sqlmock.NewResult(1, 1))
+
+// 				mock.ExpectExec(`INSERT INTO stocks \(product_id, type, quantity, created_at\)`).
+// 					WithArgs(1, "in", 10, sqlmock.AnyArg()).
+// 					WillReturnResult(sqlmock.NewResult(1, 1))
+
+// 				mock.ExpectCommit()
+// 			},
+// 			expectedErr: nil,
+// 		},
+// 		{
+// 			name: "Insufficient stock on out",
+// 			input: &models.StockRequest{
+// 				ProductID: 1,
+// 				Type:      "out",
+// 				Quantity:  10,
+// 			},
+// 			setupMocks: func() {
+
+// 				mock.ExpectQuery(`SELECT stock FROM products WHERE id = \?`).
+// 					WithArgs(1).
+// 					WillReturnRows(sqlmock.NewRows([]string{"stock"}).AddRow(5))
+// 			},
+// 			expectedErr: fmt.Errorf("insufficient stock"),
+// 		},
+// 		{
+// 			name: "Invalid stock type",
+// 			input: &models.StockRequest{
+// 				ProductID: 1,
+// 				Type:      "invalid",
+// 				Quantity:  10,
+// 			},
+// 			setupMocks:  func() {},
+// 			expectedErr: fmt.Errorf("invalid stock type"),
+// 		},
+// 		{
+// 			name: "Failed to update product stock",
+// 			input: &models.StockRequest{
+// 				ProductID: 1,
+// 				Type:      "in",
+// 				Quantity:  10,
+// 			},
+// 			setupMocks: func() {
+
+// 				mock.ExpectQuery(`SELECT stock FROM products WHERE id = \?`).
+// 					WithArgs(1).
+// 					WillReturnRows(sqlmock.NewRows([]string{"stock"}).AddRow(5))
+
+// 				mock.ExpectBegin()
+
+// 				mock.ExpectExec(`UPDATE products SET stock = \?, updated_at = \? WHERE id = \?`).
+// 					WithArgs(15, sqlmock.AnyArg(), 1).
+// 					WillReturnError(fmt.Errorf("failed to update product stock"))
+
+// 				mock.ExpectRollback()
+// 			},
+// 			expectedErr: fmt.Errorf("failed to update product stock: failed to update product stock"),
+// 		},
+// 	}
+
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			tt.setupMocks()
+
+// 			err := repo.Update(tt.input)
+
+// 			if tt.expectedErr != nil {
+// 				assert.Error(t, err)
+// 				assert.Equal(t, tt.expectedErr.Error(), err.Error())
+// 			} else {
+// 				assert.NoError(t, err)
+// 			}
+
+// 			if err := mock.ExpectationsWereMet(); err != nil {
+// 				t.Errorf("There were unfulfilled expectations: %v", err)
+// 			}
 // 		})
-// 		assert.NoError(t, err)
-// 	})
-
-// 	t.Run("error when invalid stock type", func(t *testing.T) {
-// 		err := repo.Update(&models.StockRequest{
-// 			ProductID: 1,
-// 			Type:      "invalid",
-// 			Quantity:  10,
-// 		})
-// 		assert.Error(t, err)
-// 		assert.Equal(t, "invalid stock type", err.Error())
-// 	})
+// 	}
 // }
